64092d7b6c75ab85757ba436f0a8dcb0b5395264
jimmaastricht <jimmaastricht5@gmail.com>
2023-01-16 09:41:51 -0600

    multi day graphg

--- WebClass.py
+++ WebClass.py
@@ -165,7 +165,7 @@ class WebPages:
         # st.plotly_chart(fig1, use_container_width=True, sharing="streamlit", theme="streamlit")
 
         fig2 = px.histogram(self.filter_occurences(feeder_options, date_options, bird_options),
-                            x="Day.Hour", color='Common Name', # range_x=[self.min_hr, self.max_hr],
+                            x="Date Time", color='Common Name', # range_x=[self.min_hr, self.max_hr],
                             nbins=36, width=650, height=400)
         fig2['layout']['xaxis'].update(autorange=True)
         st.plotly_chart(fig2, use_container_width=True, sharing="streamlit", theme="streamlit")
