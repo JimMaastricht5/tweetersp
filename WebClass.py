fd72ac1721b54587bb633c8db119b39f3da2b851
jimmaastricht <jimmaastricht5@gmail.com>
2023-05-09 18:14:57 -0500

    experiment with new df grid option

--- WebClass.py
+++ WebClass.py
@@ -357,7 +357,7 @@ class WebPages:
             data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
             fit_columns_on_grid_load=False,
             header_checkbox_selection_filtered_only=True,
-            height=100,
+            height=1000,
             use_checkbox=True)
         # gb = GridOptionsBuilder.from_dataframe(data)
         # gb.configure_pagination(paginationAutoPageSize=True)  # Add pagination
