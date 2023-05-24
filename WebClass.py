10285133227319322c0ec507acc8681e1cf90816
Jim Maastricht <jimmaastricht5@gmail.com>
2023-05-24 13:44:54 -0500

    add twitter timeline embedded page

--- WebClass.py
+++ WebClass.py
@@ -285,6 +285,12 @@ class WebPages:
         st.plotly_chart(fig1, use_container_width=True, sharing="streamlit", theme="streamlit")
         return
 
+    def twitter_timeline_page(self):
+        st.write('Twitter Timeline for @TweetersSP')
+        st.write('<a class="twitter-timeline" href="https://twitter.com/TweetersSp?ref_src=twsrc%5Etfw">Tweets by TweetersSp</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',
+        unsafe_allow_html=True)
+        return
+
     def about_page(self):
         st.write('About Page')
         st.write(f'This site display data for the the days from '
---
+++ pages/5_Twitter_Timeline.py
@@ -0,0 +1,5 @@
+import WebClass
+
+# init class and call main page
+webpage = WebClass.WebPages()
+webpage.twitter_timeline_page()
