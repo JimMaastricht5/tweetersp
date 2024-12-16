9abce586a4c2c58bf20f6f5f689dfda5fdd76572
jimmaastricht <jimmaastricht5@gmail.com>
2024-12-16 07:52:08 -0600

    clean up trends chart

--- WebClass.py
+++ WebClass.py
@@ -400,8 +400,8 @@ class WebPages:
         st.header(f'Daily History - May 9th 2023 to Present')
         df = self.load_daily_history()
         df = df[df['counts'] > filter_birds_cnt]
-        df['Year-Day'] = df['Year'].astype(str).str[:-2].replace(',', '') + '.' + df['Day_of_Year'].astype(str).str[:-2]
         df['Year'] = df['Year'].astype(str).str[:-2]
+        df['Year-Day'] = df['Year'] + '.' + df['Day_of_Year'].astype(str).str[:-2]
         df = df.sort_values('Year-Day')
         st.write(f'Trend of Bird Visits by Day.  Data started being retained on May 9th 2023.')
         fig1 = px.line(data_frame=df, x="Year-Day", y="counts", color='Common Name', width=650, height=800,
