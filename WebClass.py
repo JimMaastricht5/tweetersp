8ce6afe047c444ceb2c15924d2a90c31ed63be69
jimmaastricht <jimmaastricht5@gmail.com>
2025-01-19 11:00:02 -0600

    add 2024 page

--- WebClass.py
+++ WebClass.py
@@ -454,7 +454,7 @@ class WebPages:
         df_raw = pd.read_csv(df_file_name)
         df_raw['DateTime'] = pd.to_datetime(df_raw['DateTime'], errors='raise')
         df = df_raw[df_raw['DateTime'].dt.year == 2024].copy()  # .copy() avoids warnings about setting values on slice
-        df = df.rename(columns={'Name : Species'})
+        df = df.rename(columns={'Name' : 'Species'})
         df.index.name = 'Image Number'
 
 
